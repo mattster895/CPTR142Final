21f10fcb488161cdf4a7039cf9d1e25776671bf8
justarusk <justinrusk18@gmail.com>
2015-03-05 12:07:42 -0800

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -12,12 +12,14 @@ using namespace std;
 
 int main()
 {
-    ;
     ifstream fin;
     string saveLocation = "charbits.txt";
-    int size = 0;
-    char a[2028];
-
+    int i=0, k, size = 0, c[71], z=0, y=0;
+    char a[2028], b[28];
+    for(y=0; y<=70; y++)
+    {
+        c[y] = 0;
+    }
     fin.open(saveLocation.c_str());
     if(!fin)
     {
@@ -33,5 +35,40 @@ int main()
         size++;
     }
     fin.close();
-    cout << a;
+    for (i = 0; i<=2027; i++)
+        cout << a[i];
+    for (i =0; i<=27; i++)
+    {
+
+        b[i] = a[i];
+    }
+    k = i;
+    while (k<= 1972)
+    {
+       for(i=0; i<=27; i++)
+       {
+           if(b[i] != a[k+i])
+           {
+               i=28;
+               k++;
+           }
+            else if(i==27)
+            {
+                c[z] = k;
+                z++;
+                k = k + 28;
+            }
+       }
+    }
+       for(i=0; i<= 70; i++)
+       {
+        if(c[i] == 0)
+       {
+           i==71;
+       }
+       else
+       {
+           cout << "\n pattern found at position: " << c[i];
+       }
+       }
 }
