3b0523468d35691d4e425718a4cce934130ea532
mattster895 <matthew.fennell@wallawalla.edu>
2015-03-10 11:30:13 -0700

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
-#include <stdint.h>
+#include <ctime>
 
 
 //program startup
@@ -12,15 +12,20 @@ using namespace std;
 
 int main()
 {
+    clock_t t1, t2;
     ifstream fin;
     string saveLocation = "charbits.txt";
-    int i=0, k, size = 0, c[71], z=0, y=0;
+    int i=0, k, size = 0, c[71], d[71] z=0, y=0; //i is an all purpose counter variable, k is used for tracking solutions, size prevents overflow, c stores solutions, z and y do something
     char a[2028], b[28];
     for(y=0; y<=70; y++)
     {
         c[y] = 0;
     }
-    fin.open(saveLocation.c_str());
+    for(y=0; y<=70; y++)
+    {
+        d[y] = 0;
+    }
+    fin.open(saveLocation.c_str()); //opens the file and displays an error message if unable to do so
     if(!fin)
     {
         cout << "Failure trying to read file: ";
@@ -34,14 +39,15 @@ int main()
         size++;
     }
     fin.close();
-    for (i = 0; i<=2027; i++)
-        cout << a[i];
+    t1=clock();
+    for(long int q=1; q<=1000000; q++)
+    {
     for (i =0; i<=27; i++)
     {
 
         b[i] = a[i];
     }
-    k = i;
+    k = 27;
     while (k<= 1971)
     {
        for(i=0; i<=27; i++)
@@ -59,7 +65,8 @@ int main()
             }
        }
     }
-       for(i=0; i<= 70; i++)
+    }
+    for(i=0; i<= 70; i++)
        {
         if(c[i] == 0)
        {
@@ -67,7 +74,10 @@ int main()
        }
        else
        {
-           cout << "\n pattern found at position: " << c[i];
+           cout << "\n Pattern found at position: " << c[i]+1;
        }
        }
+    t2=clock();
+    cout << "\nTime difference is " << (t2-t1)/CLK_TCK << " milliseconds.";
+    return 0;
 }
