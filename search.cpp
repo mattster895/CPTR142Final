8599ec3875378cb4024b2e5952da7a909abd3a1f
CraigKyle <Beadarrian@gmail.com>
2015-03-10 11:46:33 -0700

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -2,25 +2,33 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
-#include <stdint.h>
-
-
-//program startup
-
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
+    clock_t t1, t2;
+
     ifstream fin;
     string saveLocation = "charbits.txt";
-    int i=0, k, size = 0, c[71], z=0, y=0;
+    int i=0;
+    int k;
+    int size = 0;
+    int c[71];
+    int d[71];
+    int z=0;
+    int y=0; //i is an all purpose counter variable, k is used for tracking solutions, size prevents overflow, c stores solutions, z and y do something
     char a[2028], b[28];
     for(y=0; y<=70; y++)
     {
         c[y] = 0;
     }
-    fin.open(saveLocation.c_str());
+    for(y=0; y<=70; y++)
+    {
+        d[y] = 0;
+    }
+    fin.open(saveLocation.c_str()); //opens the file and displays an error message if unable to do so
     if(!fin)
     {
         cout << "Failure trying to read file: ";
@@ -34,39 +42,52 @@ int main()
         size++;
     }
     fin.close();
-    for (i =0; i<=27; i++)
-    {
 
-        b[i] = a[i];
-    }
-    k = i;
-    while (k<= 1971)
+    t1=clock();
+
+    for(long int q=1; q<=1000000; q++)
     {
-       for(i=0; i<=27; i++)
-       {
-           if(b[i] != a[k+i])
-           {
-               i=28;
-               k++;
-           }
-            else if(i==27)
+        for (i =0; i<=27; i++)
+        {
+            b[i] = a[i];
+        }
+        k = 27;
+        while (k<=1971)
+        {
+            for(i=0; i<=27; i++)
             {
-                c[z] = k;
-                z++;
-                k = k + 28;
+                if(b[i] != a[k+i])
+                {
+                    i=28;
+                    k++;
+                }
+                else if(i==27)
+                {
+                    c[z] = k;
+                    z++;
+                    k = k + 28;
+                }
             }
-       }
+        }
     }
-       for(i=0; i<= 70; i++)
-       {
-         if(c[i] == 0)
-        {
-           i==71;
-        }
-         else
-        {
-           cout << "Pattern found at position: " << c[i] << endl;
-        }
-       }
-}
 
+    int end=1;
+    while(c[end]!=c[0])
+    {
+        end++;
+    }
+
+    end = end-1;
+
+    for(i=0; i<=end; i++)
+    {
+        cout << "Pattern found at position: " << c[i];
+        cout << endl;
+    }
+
+    t2=clock();
+
+    cout << "\nTime difference is " << (t2-t1)/CLK_TCK << " microseconds.";
+
+    return 0;
+}
