19ca013be9beb753eac917a5a892ad35a89084b4
CraigKyle <Beadarrian@gmail.com>
2015-03-12 13:58:29 -0700

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,32 +1,39 @@
+//////////////////////////////////////////////////////
+//                                                  //
+//                     CPTR142                      //
+//                     Group 4                      //
+//                  Sort Function                   //
+//                                                  //
+//                     Credits:                     //
+//                                                  //
+//            Algorithm - Craig, Quillan            //
+//         Optimization - Craig, Quillan, Mathew    //
+//              Bug Fix - Whole Group               //
+//                                                  //
+//////////////////////////////////////////////////////
+
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <string>
-#include <stdint.h>
+#include <ctime>
 
 using namespace std;
 
-extern "C"
-{
-	__inline__ uint64_t rdtsc()
-	{
-		uint32_t lo, hi;
-		/* We cannot use "=A", since this would use %rax on x86_64 */
-		__asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
-
-		return (uint64_t)hi << 32 | lo;
-	}
-}
-
 int main()
 {
-	uint64_t t1, t2;
-	double time;
+    //input and initializations start
+
+    clock_t t1,t2;
+    int time;
 
     ifstream fin;
     string saveLocation = "marbles.txt";
     int size = 0;
     char a[1999];
+    char b[1999];
+    char alpha;
 
     fin.open(saveLocation.c_str());
     if(!fin)
@@ -43,71 +50,92 @@ int main()
     }
     fin.close();
 
+    for(int z=0;z<=1999;z++)    //creates a second array, b, that is the unsorted version of a
+    {
+        b[z] = a[z];
+    }
+
     int temp1=0;
     int temp2=0;
 
-    t1 = rdtsc();
+    //input and initializations end
 
-    for(int i=1; i<=1000000; i++)
+    t1 = clock();
 
-    if(a[temp1]=='R')
+    //sorting start
+
+    for(long int i=1;i<=1000000;i++)
     {
-        temp1++;
-    }
+        for(int z=0;z<=1999;z++)
+        {
+            a[z] = b[z];
+        }
 
-    if(a[temp2]=='R')
-    {
-        temp2=temp1;
-    }
+        temp1=0;
+        temp2=0;
 
-    while(temp1!=1999)
-    {
         if(a[temp1]=='R')
         {
-            swap(a[temp1],a[temp2]);
+            temp1++;
+        }
+        if(a[temp2]=='R')
+        {
+            temp2=temp1;
+        }
+
+        while(temp1!=1999)
+        {
+            if(a[temp1]=='R')
+            {
+                swap(a[temp1],a[temp2]);
+                temp2++;
+                temp1++;
+            }
+            else
+            {
+                temp1++;
+            }
+        }
+
+        temp1=0;
+        temp2=0;
+
+        while((a[temp2]=='R')||(a[temp2]=='W'))
+        {
             temp2++;
-            temp1++;
         }
-        else
+        while((a[temp1]=='R')||(a[temp1]=='W'))
         {
-            temp1++;
+            temp1=temp2;
         }
-    }
 
-    temp1=0;
-    temp2=0;
-
-    while((a[temp2]=='R')||(a[temp2]=='W'))
-    {
-        temp2++;
-    }
-
-    while((a[temp1]=='R')||(a[temp1]=='W'))
-    {
-        temp1=temp2;
-    }
-
-    while(temp1!=1999)
-    {
-        if(a[temp1]=='W')
+        while(temp1!=1999)
         {
-            swap(a[temp1],a[temp2]);
-            temp2++;
-            temp1++;
-        }
-        else
-        {
-            temp1++;
+            if(a[temp1]=='W')
+            {
+                swap(a[temp1],a[temp2]);
+                temp2++;
+                temp1++;
+            }
+            else
+            {
+                temp1++;
+            }
         }
     }
 
-    t2 = rdtsc();
-    time = (t2-t1)/1000000;
+    //sorting end
 
-    cout << a;
-    cout << '\b' << '\b';
+    t2 = clock();
 
-    cout << "Time difference is " << time << " microseconds.";
+    for(int z=0;z<=1999;z++)    //easy for arrays to get junk in them,
+    {                           //this just outputs the first 2000 characters
+        cout << a[z];
+    }
+
+    time = (t2-t1)/CLK_TCK;
+
+    cout << "Time difference is "<< time << " microseconds.";
 
     return 0;
 }
