5e64e922f8d368abafbd5c8bb5b8608a79cecdd0
CraigKyle <Beadarrian@gmail.com>
2015-03-12 11:15:21 -0700

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -2,13 +2,26 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
-#include <ctime>
+#include <stdint.h>
 
 using namespace std;
 
+extern "C"
+{
+	__inline__ uint64_t rdtsc()
+	{
+		uint32_t lo, hi;
+		/* We cannot use "=A", since this would use %rax on x86_64 */
+		__asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+
+		return (uint64_t)hi << 32 | lo;
+	}
+}
+
 int main()
 {
-    clock_t t1, t2;
+	uint64_t t1, t2;
+	double time;
 
     ifstream fin;
     string saveLocation = "marbles.txt";
@@ -33,68 +46,68 @@ int main()
     int temp1=0;
     int temp2=0;
 
-    t1 = clock();
+    t1 = rdtsc();
 
-    for(long int i=1; i<=1000000; i++)
+    for(int i=1; i<=1000000; i++)
+
+    if(a[temp1]=='R')
+    {
+        temp1++;
+    }
+
+    if(a[temp2]=='R')
+    {
+        temp2=temp1;
+    }
+
+    while(temp1!=1999)
     {
         if(a[temp1]=='R')
         {
+            swap(a[temp1],a[temp2]);
+            temp2++;
             temp1++;
         }
-
-        if(a[temp2]=='R')
+        else
         {
-            temp2=temp1;
-        }
-
-        while(temp1!=1999)
-        {
-            if(a[temp1]=='R')
-            {
-                swap(a[temp1],a[temp2]);
-                temp2++;
-                temp1++;
-            }
-            else
-            {
-                temp1++;
-            }
-        }
-
-        temp1=0;
-        temp2=0;
-
-        while((a[temp2]=='R')||(a[temp2]=='W'))
-        {
-            temp2++;
-        }
-
-        while((a[temp1]=='R')||(a[temp1]=='W'))
-        {
-            temp1=temp2;
-        }
-
-        while(temp1!=1999)
-        {
-            if(a[temp1]=='W')
-            {
-                swap(a[temp1],a[temp2]);
-                temp2++;
-                temp1++;
-            }
-            else
-            {
-                temp1++;
-            }
+            temp1++;
         }
     }
+
+    temp1=0;
+    temp2=0;
+
+    while((a[temp2]=='R')||(a[temp2]=='W'))
+    {
+        temp2++;
+    }
+
+    while((a[temp1]=='R')||(a[temp1]=='W'))
+    {
+        temp1=temp2;
+    }
+
+    while(temp1!=1999)
+    {
+        if(a[temp1]=='W')
+        {
+            swap(a[temp1],a[temp2]);
+            temp2++;
+            temp1++;
+        }
+        else
+        {
+            temp1++;
+        }
+    }
+
+    t2 = rdtsc();
+    time = (t2-t1)/1000000;
 
     cout << a;
     cout << '\b' << '\b';
 
-    t2 = clock();
-
-    cout << "Time difference is " << (t2-t1)/CLK_TCK << " microseconds.";
+    cout << "Time difference is " << time << " microseconds.";
 
     return 0;
 }
